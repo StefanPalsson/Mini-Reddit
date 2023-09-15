5e6ea6baea528130307af61d2f71cdc94d2b4b5c
Jessika <amantalath@hotmail.com>
2023-09-15 09:39:39 +0200

    create post added

--- src/api/api.js
+++ src/api/api.js
@@ -8,7 +8,6 @@ const get = async (endpoint) => {
   const data = await response.json();
   return data;
 };
-
 const post = async (endpoint, payload) => {
   const response = await fetch(`${baseURL}${endpoint}`, {
     method: 'POST',
@@ -28,20 +27,41 @@ const getSinglePost = async (postId) => {
   return get(`/posts/${postId}`);
 };
 
-const createUser = async (username, password, email) => {
-  const payload = {
-    username,
-    password,
-    email
-  };
-  
-  return post ('/users', payload);
+// const createUser = async (username, password, email) => {
+//   const payload = {
+//     username,
+//     password,
+//     email
+//   };
+//   return post ('/users', payload);
+// }
 
-  
+
+//Create post 
+const addPost = async (result) => {
+
+  const res = await fetch(`${baseURL}/posts/add`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      title: `title`,
+      userId: `${result.id}`,
+      content: `${result.content}`
+    })
+  })
+  .then(res => res.json())
+  .then(console.log)
+  //Pass to global state?
+  // .then((post) => {
+  //     setPosts([...posts, post]);
+  // });
+}
+// temporary function to get all users
+const getAllUsers = async () => {
+  let res = await fetch('https://dummyjson.com/users')
+  const data = await res.json();
+  const userArray = data.users.map((user) => user);
+  return userArray;
 }
 
-const getUser = async (userId) => {
-  return get('/users/${userId}');
-}
-
-export { get, post, getSinglePost, createUser, getUser };
+export { get, post, getSinglePost, addPost, getAllUsers };
--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,11 +1,78 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { getAllUsers } from '../api/api';
+import { addPost } from '../api/api';
+import '../styles/createpost.css';
+import '../styles/MainPage.css';
 
-function CreatePosts() {
+/* ASSIGNMENT:
+# Huvudsidan ska också innehålla följande funktionalitet:
+# Skapa nytt inlägg
+# Denna funktionalitet implementeras genom att välja vilken användare som ska skapa inlägget. 
+# Detta är inte realistiskt och inte hur reddit fungerar eftersom man vanligtvis är inloggad på en användare, men det är så uppgiften är. Vid skapandet av inlägget ska man alltså få välja vilken användare (utav de som API:et innehåller) som skapar inlägget.
+# Ni skall anroppa API:et för att skapa nya inlägg (se endpoint)
+# Tänk på att API:et inte faktiskt ändrar inlägg-databasen så ni måste hantera det lokalt
+
+TODO change props passed to this function
+*/
+function CreatePost() {
+    const [user, setUser] = useState([]);
+    const [inputs, setInputs] = useState({});
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        addPost(inputs)
+        //clear inputfields 
+        setInputs({})
+    };
+    const handleChange = (event) => {
+        console.log(event.target.value);
+        const name = event.target.name;
+        const value = event.target.value;
+        setInputs((values) => ({ ...values, [name]: value } ));
+    };
+    useEffect(() => {
+        getAllUsers().then(setUser);
+    }, []);
+
     return (
-        <div>
-            {}
-        </div>
+        <div className='form-container'>
+            <form className="form" onSubmit={handleSubmit}>
+                <h2>Create Post</h2>
+                <label>
+            Title:
+            <input
+                type="text"
+                name="title"
+                required
+                value={inputs.title || ""}
+                onChange={handleChange}
+            />
+            </label>
+            <label>
+            Content:
+            <textarea
+                type="text"
+                name="content"
+                rows="4" cols="50"
+                required
+                value={inputs.content || ""}
+                onChange={handleChange}
+            />
+            </label>
+            <label>
+            User: <br/>            
+            <select name="id" onChange={handleChange}>
+                <option defaultValue hidden> {'-----------------'} </option>
+                {user.map((user) =>
+                    <option key={user.id} value={user.id || ""}> { user.firstName }</option>)}
+            </select>
+            </label>                    
+            <div className='submitBtn'>
+                <input type="submit" />
+            </div>
+        </form>
+    </div>
     );
 }
 
-export default CreatePosts;
+export default CreatePost;
